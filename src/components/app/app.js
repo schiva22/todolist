7666782fdfa8ce5c8672c19cf690cfc9c727cb33
Shalagin Ivan <sch.ivan22@gmail.com>
2021-01-21 16:27:03 +0700

    Added the ability to add data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
 export default class App extends Component {
+
+  maxId = 100;
 
   state = {
     todoData: [
@@ -35,6 +38,27 @@ export default class App extends Component {
     });
   };
 
+  addItem = (text) => {
+    //generate id
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+
+    //add element in array
+    this.setState(({ todoData }) => {
+      const newArr =[
+        ...todoData,
+        newItem
+      ];
+
+      return {
+        todoData: newArr
+      };
+    });
+  };
+
   render() {
     return (
       <div className="todo-app">
@@ -46,6 +70,8 @@ export default class App extends Component {
   
         <TodoList todos={this.state.todoData} 
         onDeleted={ this.deleteItem } />
+
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );  
   };
